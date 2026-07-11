f31ee002b672f0aa5d8f11121c053f8745708fc5
ayalhrr905-afk <ayalhrr905@gmail.com>
2026-07-11 14:07:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function celebrate() {
-    // تشغيل تأثير الألوان المتساقطة
+    // 1. تشغيل الموسيقى
+    var audio = document.getElementById("birthday-audio");
+    audio.play().catch(function(error) {
+        console.log("المتصفح حجب التشغيل التلقائي حتى يتفاعل المستخدم:", error);
+    });
+
+    // 2. تشغيل تأثير الألوان المتساقطة
     confetti({
         particleCount: 150,
         spread: 80,
@@ -7,7 +13,7 @@ function celebrate() {
     });
 }
 
-// تشغيل التأثير تلقائياً أول ما يفتح الموقع كمان!
+// عند فتح الصفحة لأول مرة (تأثير ألوان بدون صوت عشان قيود المتصفح)
 window.onload = function() {
     confetti({
         particleCount: 100,
